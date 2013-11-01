7b61e2dc45a3a01cf31e2bac596bb18ba04b64cb
aaHung <landxh@gmail.com>
2013-11-02 01:05:39 +0800

    more class style, ruin global variables

--- source/class.js
+++ source/class.js
@@ -1,72 +1,127 @@
-function boxCollection(){
+//This file includes Listener, BoxCollection and PreviewBox three classes.
+
+// Listener is the trigger to everything. 
+//and the jump out is set in listenKeys(), 
+//because this is the smallest events so that saves resources
+
+function Listener(boxCol){
+	this.hoverLink;
+	this.mouseX;
+	this.mouseY;
+	this.boxCol = boxCol;
+}
+	Listener.prototype.start = function(){
+		this.listenCursor();
+		this.listenKeys();
+		this.listenLinks();
+	}
+	Listener.prototype.listenCursor = function(){
+		$(window).mousemove(function(e){
+			this.mouseX = e.clientX;
+			this.mouseY = e.clientY;
+		});
+	}
+	Listener.prototype.listenKeys = function(){
+		var boxCol = this.boxCol; //change to local variable
+		$(window).on("keypress", function(e){
+			var key = e.which;
+			if (key == 119 && hoverLink != null) {
+				if (boxCol.member.length == 0 || boxCol.topBox().src != $(hoverLink).attr('href')){
+					var newBox = new PreviewBox($(hoverLink).attr('href'));
+					boxCol.add(newBox);
+					newBox.render();
+				}
+				else {
+					boxCol.pop();
+				}
+			}
+			if (key == 119 && hoverLink == null && boxCol.member.length != 0) {
+				boxCol.pop();
+			}
+		});
+	}
+	Listener.prototype.listenLinks = function(){
+		$("a").hover(function(){
+			hoverLink = this; 
+		}, function(){
+			hoverLink = null;
+		});
+	}
+
+
+
+function BoxCollection(){
 	this.topLayer = 0;
 	this.member = [];
 }
-boxCollection.prototype.add = function(box){
-	this.member.push(box);
-	box.layer = this.topLayer + 1;
-	this.detect();
-}
-boxCollection.prototype.remove = function(box){
-	this.member.pop(box);
-	this.detect();
-}
-boxCollection.prototype.detect = function(){
-	var topLayer = 0;
-	for (var i = 0; i < this.member.length; i ++){
-		if (this.member[i].layer > topLayer)
-			topLayer = this.member[i].layer;
+	BoxCollection.prototype.add = function(box){
+		this.member.push(box);
+		box.layer = this.topLayer + 1;
+		this.detect();
 	}
-	this.topLayer = topLayer;
-}
-boxCollection.prototype.pop = function(){
-	var topBox = this.topBox();
-	this.member.pop(topBox);
-	topBox.destroy();
-	this.detect();
-}
-boxCollection.prototype.topBox = function(){
-	var boxes = this.member;
-	var topLayer = this.topLayer;
-	var topBox = $.grep(boxes, function(n, i){
-		return n.layer == topLayer;
-	});
-	return topBox[0];
-}
-function previewBox(src){
+	BoxCollection.prototype.remove = function(box){
+		this.member.pop(box);
+		this.detect();
+	}
+	BoxCollection.prototype.detect = function(){
+		var topLayer = 0;
+		for (var i = 0; i < this.member.length; i ++){
+			if (this.member[i].layer > topLayer)
+				topLayer = this.member[i].layer;
+		}
+		this.topLayer = topLayer;
+	}
+	BoxCollection.prototype.pop = function(){
+		var topBox = this.topBox();
+		this.member.pop(topBox);
+		topBox.destroy();
+		this.detect();
+	}
+	BoxCollection.prototype.topBox = function(){
+		var boxes = this.member;
+		var topLayer = this.topLayer;
+		var topBox = $.grep(boxes, function(n, i){
+			return n.layer == topLayer;
+		});
+		return topBox[0];
+	}
+
+
+
+function PreviewBox(src){
 	this.src = src;
 	this.layer;
 	this.ele;
 }
-previewBox.prototype.render = function(){
-	// get browser size
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	var ele = document.createElement("iframe");//create preview box element, for future render
-	this.ele = ele;
-	$(ele).attr("src", this.src);
-	$(ele).addClass("previewBox");//set identity
-	$(ele).attr("data-layer", this.layer);//set identity
-	// judge the mouse position and set the position of preview box dynamically
-	if (mouseX < windowWidth / 2){
-		$(ele).css("right", 10* this.layer);
+	PreviewBox.prototype.render = function(){
+		// get browser size
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		var ele = document.createElement("iframe");//create preview box element, for future render
+		this.ele = ele;
+		$(ele).attr("src", this.src);
+		$(ele).addClass("PreviewBox");//set identity
+		$(ele).attr("data-layer", this.layer);//set identity
+		// judge the mouse position and set the position of preview box dynamically
+		if (mouseX < windowWidth / 2){
+			$(ele).css("right", 10* this.layer);
+		}
+		else{
+			$(ele).css("left", 10* this.layer);
+		}
+		if (mouseY < windowHeight / 2){
+			$(ele).css("bottom", 10* this.layer);
+		}
+		else{
+			$(ele).css("top", 10* this.layer);
+		}
+		$(ele).css({"height": "400px", "width": "700px", "position": "fixed", "border": "0px", "z-index": "999999", "box-shadow": "0px 0px 10px 0px black", "background": "url('http://dribbble.s3.amazonaws.com/users/80078/screenshots/995621/loading.gif')", "background-position": "center center"});
+		$(ele).load(function(){
+			$(this).css("background", "white");
+		});
+		$("body").append(ele);//render the preview box element
+		$("body").blur();
 	}
-	else{
-		$(ele).css("left", 10* this.layer);
+	PreviewBox.prototype.destroy = function(){
+		this.ele.remove();
 	}
-	if (mouseY < windowHeight / 2){
-		$(ele).css("bottom", 10* this.layer);
-	}
-	else{
-		$(ele).css("top", 10* this.layer);
-	}
-	$(ele).css({"height": "400px", "width": "700px", "position": "fixed", "border": "0px", "z-index": "999999", "box-shadow": "0px 0px 10px 0px black", "background": "url('http://dribbble.s3.amazonaws.com/users/80078/screenshots/995621/loading.gif')", "background-position": "center center"});
-	$(ele).load(function(){
-		$(this).css("background", "white");
-	});
-	$("body").append(ele);//render the preview box element
-	$("body").blur();
-}
-previewBox.prototype.destroy = function(){
-	this.ele.remove();
-}
--- source/main.js
+++ source/main.js
@@ -1,56 +1,6 @@
-// get the mouse position
-var mouseX, mouseY;
-var boxCol;
-var hoverLink;
 $(function(){
-	boxCol = new boxCollection();
-	$(window).mousemove(function(e){
-		mouseX = e.clientX;
-		mouseY = e.clientY;
-	});
-});
-// main function
-function listenLinks(){
-	$("a").hover(function(){
-		hoverLink = this; //save this to a for future usage
-	}, function(){
-		hoverLink = null;
-	});
-}
-
-$(function(){
-	listenKeys();
-	listenLinks();
+	var boxCol = new BoxCollection();
+	var listener = new Listener(boxCol);
+	listener.start();
 });
 
-function listenKeys(){
-	$(window).on("keypress", function(e){
-		var key = e.which;
-		if (key == 119 && hoverLink != null) {
-			if (boxCol.member.length == 0){
-				var newBox = new previewBox($(hoverLink).attr('href'));
-				boxCol.add(newBox);
-				newBox.render();
-			}
-			else {
-				if (boxCol.topBox().src == $(hoverLink).attr('href')) {
-					boxCol.pop();
-				}
-				else {
-					var newBox = new previewBox($(hoverLink).attr('href'));
-					boxCol.add(newBox);
-					newBox.render();
-				}
-			}
-		}
-		if (key == 119 && hoverLink == null)  {
-			if (boxCol.member.length == 0){
-				;
-			}
-			else {
-				boxCol.pop();
-				debugger;
-			}
-		}
-	});
-}
