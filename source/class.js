94051aafddcda7bdf50b8c690655427a7e926cf9
aaHung <landxh@gmail.com>
2013-11-01 16:44:05 +0800

    change to class style

---
+++ source/class.js
@@ -0,0 +1,71 @@
+function boxCollection(){
+	this.topLayer = 0;
+	this.member = [];
+}
+boxCollection.prototype.add = function(box){
+	this.member.push(box);
+	box.layer = this.topLayer + 1;
+	this.detect();
+}
+boxCollection.prototype.remove = function(box){
+	this.member.pop(box);
+	this.detect();
+}
+boxCollection.prototype.detect = function(){
+	var topLayer = 0;
+	for (var i = 0; i < this.member.length; i ++){
+		if (this.member[i].layer > topLayer)
+			topLayer = this.member[i].layer;
+	}
+	this.topLayer = topLayer;
+}
+boxCollection.prototype.pop = function(){
+	var topBox = this.topBox();
+	this.member.pop(topBox);
+	topBox.destroy();
+	this.detect();
+}
+boxCollection.prototype.topBox = function(){
+	var boxes = this.member;
+	var topLayer = this.topLayer;
+	var topBox = $.grep(boxes, function(n, i){
+		return n.layer == topLayer;
+	});
+	return topBox[0];
+}
+function previewBox(src){
+	this.src = src;
+	this.layer;
+	this.ele;
+}
+previewBox.prototype.render = function(){
+	// get browser size
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+	var ele = document.createElement("iframe");//create preview box element, for future render
+	this.ele = ele;
+	$(ele).attr("src", this.src);
+	$(ele).addClass("previewBox");//set identity
+	$(ele).attr("data-layer", this.layer);//set identity
+	// judge the mouse position and set the position of preview box dynamically
+	if (mouseX < windowWidth / 2){
+		$(ele).css("right", 10* this.layer);
+	}
+	else{
+		$(ele).css("left", 10* this.layer);
+	}
+	if (mouseY < windowHeight / 2){
+		$(ele).css("bottom", 10* this.layer);
+	}
+	else{
+		$(ele).css("top", 10* this.layer);
+	}
+	$(ele).css({"height": "400px", "width": "700px", "position": "fixed", "border": "0px", "z-index": "999999", "box-shadow": "0px 0px 10px 0px black", "background": "url('http://dribbble.s3.amazonaws.com/users/80078/screenshots/995621/loading.gif')", "background-position": "center center"});
+	$(ele).load(function(){
+		$(this).css("background", "white");
+	});
+	$("body").append(ele);//render the preview box element
+}
+previewBox.prototype.destroy = function(){
+	this.ele.remove();
+}
--- source/main.js
+++ source/main.js
@@ -1,64 +1,56 @@
 // get the mouse position
 var mouseX, mouseY;
+var boxCol;
+var hoverLink;
 $(function(){
+	boxCol = new boxCollection();
 	$(window).mousemove(function(e){
 		mouseX = e.clientX;
 		mouseY = e.clientY;
 	});
 });
 // main function
-function previewIt(){
+function listenLinks(){
 	$("a").hover(function(){
-		var a = this; //save this to a for future usage
-		$(window).off("keypress");//end the bind of keypress event created by previous code
-		$(window).keypress(function(e) {
-			var key = e.which;
-			if (key == 119) {
-				if ($(".previewBox").length == 0)
-					createPreview(a); //pass mouse position and "this" reference to function
-				else {
-					$('.previewBox').remove(); //if there does exsit preview box, remove it. Why doing this is sometimes user wants to close the preview even the mouse still on the link
-				}
-			}
-		});
+		hoverLink = this; //save this to a for future usage
 	}, function(){
-		$(window).off("keypress");//end the bind of keypress event created by previous code
-		$(window).keypress(function(e) {
-			var key = e.which;
-			if (key == 119) { //119 is "w"
-				if ($(".previewBox").length > 0){
-					$('.previewBox').remove();//if there exist preview boxes, destroy it
+		hoverLink = null;
+	});
+}
+
+$(function(){
+	listenKeys();
+	listenLinks();
+});
+
+function listenKeys(){
+	$(window).on("keypress", function(e){
+		var key = e.which;
+		if (key == 119 && hoverLink != null) {
+			if (boxCol.member.length == 0){
+				var newBox = new previewBox($(hoverLink).attr('href'));
+				boxCol.add(newBox);
+				newBox.render();
+			}
+			else {
+				if (boxCol.topBox().src == $(hoverLink).attr('href')) {
+					boxCol.pop();
+				}
+				else {
+					var newBox = new previewBox($(hoverLink).attr('href'));
+					boxCol.add(newBox);
+					newBox.render();
 				}
 			}
-		});
-	});	
-}
-
-function createPreview(a){
-	// get browser size
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	var ele = document.createElement("iframe");//create preview box element, for future render
-	$(ele).css({"height": "400px", "width": "700px", "position": "fixed", "border": "0px", "z-index": "999999", "box-shadow": "0px 0px 10px 0px black", "background": "url('http://dribbble.s3.amazonaws.com/users/80078/screenshots/995621/loading.gif')", "background-position": "center center"});
-	// judge the mouse position and set the position of preview box dynamically
-	if (mouseX < windowWidth / 2){
-		$(ele).css("right", "10px");
-	}
-	else{
-		$(ele).css("left", "10px");
-	}
-	if (mouseY < windowHeight / 2){
-		$(ele).css("bottom", "10px");
-	}
-	else{
-		$(ele).css("top", "10px");
-	}
-	$(ele).attr("src", $(a).attr("href"));
-	$(ele).addClass("previewBox");//set identity
-	$(ele).load(function(){
-		$(this).css("background", "white");
+		}
+		if (key == 119 && hoverLink == null)  {
+			if (boxCol.member.length == 0){
+				;
+			}
+			else {
+				boxCol.pop();
+				debugger;
+			}
+		}
 	});
-	$(a).parent().append(ele);//render the preview box element
 }
-previewIt();
-
