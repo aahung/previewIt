b41ae1402f773d4ceca5032ad736e3c1c35a6d6c
aaHung <landxh@gmail.com>
2013-11-10 18:32:14 +0800

    remove check update

--- js/main.js
+++ js/main.js
@@ -3,19 +3,4 @@ $(function(){
 	boxColFuckGlobalVariable = new BoxCollection();
 	var listener = new Listener();
 	listener.start();
-	$.ajax({ 
-		url: 'http://ideati.me/sep/p/check.php',
-		type: 'POST',
-		timeout: 500,
-		data: "check update",
-     })
-	.done(function(data) {
-		console.log(data);
-		if (data.substring(0, 3) != '"hi'){
-			alert(data);
-		}
-	})
-	.fail(function(jqXHR, textStatus, errorThrown){
-		console.log('fail to check update.');
-	});
 });
